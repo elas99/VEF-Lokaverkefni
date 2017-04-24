1593d149191aebe5d9260969bc001c6ac7bcb41b
elas99 <elas99@hotmail.com>
2017-04-24 15:29:47 +0000

    Add files via upload
    Hover bætt við

--- js.js
+++ js.js
@@ -9,16 +9,16 @@ $("#test").append(tbl);
 for(var i=0;i<response.results.length;i++)
 {
 	
-    var tr="<tr>";
-    var td1="<td>"+response.results[i]["dateOfShow"]+"</td>";
-    var td2="<td>"+response.results[i]["eventDateName"]+"</td>";
-    var td3="<td>"+response.results[i]["eventHallName"]+"</td></tr>";
-    var td4="<td>"+response.results[i]["imageSource"]+"</td>";
-    var td5="<td>"+response.results[i]["name"]+"</td>";
-    var td6="<td>"+response.results[i]["userGroupName"]+"</td></tr>";
-
+    var divop = "<div>"
+    var td1="<div>"+response.results[i]["dateOfShow"]+"</div> ";
+    var td2="<div>"+response.results[i]["eventDateName"]+"</div>";
+    var td3="<div>"+response.results[i]["eventHallName"]+"</div>";
+    var td4="<div class='imgWrap'><img src='"+response.results[i]['imageSource']+"'><p class='imgDescription'>"+response.results[i]['name']+"</p>";
+    var td5=""+response.results[i]["name"]+"";
+    var td6="<div>"+response.results[i]["userGroupName"]+"</div>";
+    var divlok = "</div>"
     
-   $("#mytable").append(tr+td1+td2+td3+td4+td5+td6); 
+   $("#mytable").append(divop+td4+divlok); 
   
 }
 
